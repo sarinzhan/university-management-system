fce69f9186c937e9e08d762c95edccd0ea124de7
Sarinzhan <sarinzhanakazbekov@gmail.com>
2024-05-01 23:08:19 +0600

    Test commit

--- src/main/java/com/example/universitymanagementsystem/controller/ApplicantController.java
+++ src/main/java/com/example/universitymanagementsystem/controller/ApplicantController.java
@@ -32,6 +32,5 @@ public class ApplicantController {
             return responseDto;
         }
         return responseDto;
-
     }
 }
---
+++ src/main/java/com/example/universitymanagementsystem/controller/VerificationController.java
@@ -0,0 +1,9 @@
+package com.example.universitymanagementsystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/verification-token")
+public class VerificationController {
+}
