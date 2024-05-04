37a0bdea9ffd63e4ab88792fa37cb5f3a9ec6f82
Sarinzhan <sarinzhanakazbekov@gmail.com>
2024-05-05 00:58:56 +0600

    add annotation to controller 1 and 2

--- src/main/java/com/example/universitymanagementsystem/controller/ControllerFromFeature2.java
+++ src/main/java/com/example/universitymanagementsystem/controller/ControllerFromFeature2.java
@@ -1,4 +1,7 @@
 package com.example.universitymanagementsystem.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ControllerFromFeature2 {
 }
