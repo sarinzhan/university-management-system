3a6351c4c6d5774e7ed92402ed2fd0469b339a0f
Sarinzhan <sarinzhanakazbekov@gmail.com>
2024-05-05 01:10:52 +0600

    Add endPoint in controller

--- src/main/java/com/example/universitymanagementsystem/controller/ControllerFromFeature1.java
+++ src/main/java/com/example/universitymanagementsystem/controller/ControllerFromFeature1.java
@@ -1,7 +1,12 @@
 package com.example.universitymanagementsystem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerFromFeature1 {
+    @GetMapping
+    public String getMa(){
+        return "Hello";
+    }
 }
