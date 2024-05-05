19edfed763c1cee21babaf4c69b5b6da22883c05
Sarinzhan <sarinzhanakazbekov@gmail.com>
2024-05-05 17:04:26 +0600

    delete unnecessary controller and add GetMapping(check)

--- src/main/java/com/example/universitymanagementsystem/controller/VerificationController.java
+++ src/main/java/com/example/universitymanagementsystem/controller/VerificationController.java
@@ -1,12 +1,14 @@
 package com.example.universitymanagementsystem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/verification-token")
 public class VerificationController {
-    public static void main(String[] args) {
-            int a = 1;
+    @GetMapping("/check")
+    public Boolean check(){
+        return true;
     }
 }
