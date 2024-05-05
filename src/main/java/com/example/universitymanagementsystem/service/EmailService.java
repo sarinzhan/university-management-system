becc855dba879e0aa1ee6c7ea988f399dc173e82
rigbeats <118600193+rigbeats@users.noreply.github.com>
2024-05-05 16:28:50 +0500

    Revert "подтвверждение по электронной почте"

--- src/main/java/com/example/universitymanagementsystem/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.universitymanagementsystem.service;
-
-import javax.mail.MessagingException;
-
-public interface EmailService {
-    void sendMessage(String email, String subject, String message) throws ErrorMessageException, MessagingException;
-}
