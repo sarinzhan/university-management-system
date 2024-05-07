e8fe0e60f579d698767b1410daa06bcb55869f0e
Sarinzhan <sarinzhanakazbekov@gmail.com>
2024-05-07 17:31:07 +0600

    delete EmailCodeService

--- src/main/java/com/example/universitymanagementsystem/service/EmailCode.java
+++
@@ -1,7 +0,0 @@
-package com.example.universitymanagementsystem.service;
-
-public interface EmailCode {
-    void createCode(String email,String code);
-    void compare(String email,String code);
-
-}
