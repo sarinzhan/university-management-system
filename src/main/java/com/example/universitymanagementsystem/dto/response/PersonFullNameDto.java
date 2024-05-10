7b545463555228b8119304d9f0598ac33f748e72
Sarinzhan <123740565+sarinzhan@users.noreply.github.com>
2024-05-10 15:03:57 +0600

    Revert "Ums 3 zapolnit danny"

---
+++ src/main/java/com/example/universitymanagementsystem/dto/response/PersonFullNameDto.java
@@ -0,0 +1,12 @@
+package com.example.universitymanagementsystem.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class PersonFullNameDto{
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+}
